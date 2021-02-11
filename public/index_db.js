05b192097c189463e4f565d6429d78925f1dd99c
MJRudolph <ReindeerCode@gmail.com>
2021-02-10 20:01:34 -0500

    indexedDB refactoring

--- public/index_db.js
+++ public/index_db.js
@@ -5,28 +5,28 @@ request.onupgradeneeded = (event) => {
   const db = event.target.result;
 
   // Creates an object store with a amountID keypath that can be used to query on.
-  const toDoListStore = db.createObjectStore("budget_DB", {
+  const budgetStore = db.createObjectStore("budget_DB", {
     keyPath: "amountID",
   });
   // Creates a statusIndex that we can query on.
-  toDoListStore.createIndex("statusIndex", "status");
+  budgetStore.createIndex("statusIndex", "status");
 };
 
 // Opens a transaction, accesses the budget_DB objectStore and statusIndex.
 request.onsuccess = () => {
   const db = request.result;
   const transaction = db.transaction(["budget_DB"], "readwrite");
-  const toDoListStore = transaction.objectStore("budget_DB");
-  const statusIndex = toDoListStore.index("statusIndex");
+  const budgetStore = transaction.objectStore("budget_DB");
+  const statusIndex = budgetStore.index("statusIndex");
 
   // Adds data to our objectStore
-  toDoListStore.add({
-    amountID: " how do i get the id value here????????",
-    status: " how do i get the id value here?????????",
+  budgetStore.add({
+    amountID: " how do i get the id=t-name value here????????",
+    status: " how do i get the id=t-amount value here?????????",
   });
 
   // Return an item by keyPath
-  const getRequest = toDoListStore.get("1");
+  const getRequest = budgetStore.get("1");
   getRequest.onsuccess = () => {
     console.log(getRequest.result);
   };
@@ -37,30 +37,3 @@ request.onsuccess = () => {
     console.log(getRequestIdx.result);
   };
 };
-
-// if (!("indexedDB" in window)) {
-//   console.log("Indexed not in windows");
-// }
-// console.log("Indexed in windows");
-// let request = indexedDB.open("budget_db", 1);
-
-// // console.log(request);
-
-// request.onupgradeneeded = (event) => {
-//   console.log("I am in the Db logic");
-//   let db = event.target.result;
-//   let object_store = db.createObjectStore("transactions", { keyPath: "tname" });
-// };
-
-// request.onsuccess = (event) => {
-//   console.log("I am in the success logic");
-//   let db = request.result;
-//   let transaction = db.transaction("transactions", "readwrite");
-//   console.log(transaction);
-//   let object_store = transaction.objectStore("transactions");
-
-//   object_store.add({
-//     tname: "????",
-//     amount: 1.45,
-//   });
-// };
